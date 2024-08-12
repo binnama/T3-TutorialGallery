d9b6506f0cb3dbe9d5689b6459c0c49ae852a1c0
binnama <laasestad@yahoo.no>
2024-08-12 13:36:37 +0200

    Add shad sonner/toast component

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/navigation"; 
 import { useUploadThing } from "~/utils/uploadthing";
+import { toast } from "sonner";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -50,8 +51,16 @@ export function SimpleUploadButton() {
   const router = useRouter(); // Remember, navigation, not router
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", {
+        duration: 100000,
+        id: "upload-begin",
+      });
+    },
     onClientUploadComplete() {
-        router.refresh();
+      toast.dismiss("upload-begin");
+      toast("Upload complete!");
+      router.refresh();
     },
   });
 
