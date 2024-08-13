6f8cf25f71072b63b3c6ba8f95eb02aee9f60843
binnama <laasestad@yahoo.no>
2024-08-13 18:25:17 +0200

    Analytics on delete button

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -40,5 +41,13 @@ export async function deleteImage(id: number) {
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
 
-    redirect("/"); // Sends the user back home
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
+
+  redirect("/"); // Sends the user back home
 }
